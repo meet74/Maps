3f7b55cb64297dd724312d925c85954e34dbbf9a
meet74 <meet237@gmail.com>
2023-12-01 17:23:39 -0500

    Initial Commit

---
+++ Maps/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Maps
+//
+//  Created by Meet Patel on 2023-12-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
